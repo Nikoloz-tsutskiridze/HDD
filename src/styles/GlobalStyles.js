05dfc6d0bfbf919e267c2bc497e279087fab626b
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-09-25 19:35:07 +0400

    Routes

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,15 +1,6 @@
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
 
 /* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
 
@@ -84,7 +75,7 @@ html {
 }
 
 body {
-  font-family: "Poppins", sans-serif;
+  font-family: "Permanent Marker", cursive;
   color: var(--color-grey-700);
 
   transition: color 0.3s, background-color 0.3s;
@@ -194,3 +185,5 @@ FOR DARK MODE
 --image-grayscale: 10%;
 --image-opacity: 90%;
 */
+`;
+export default GlobalStyles;

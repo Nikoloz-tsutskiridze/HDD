5ef41ba2f7eb5a26ccf8d3d31c28f3fd920df2fb
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-10-07 22:34:17 +0400

    Dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useResetBookings } from "./useRecetBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,23 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading } = useResetBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading || isLoading2) return <Spinner />;
+
+  console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Todays activity</div>
+      <div>chart stay</div>
+      <div>chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
